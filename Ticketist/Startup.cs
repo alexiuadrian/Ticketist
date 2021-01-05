fbec63bdf9c561c1fbd415fa573bfa35b2de3bb2
Alexiu Adrian <48868394+alexiuadrian@users.noreply.github.com>
2021-01-06 00:04:59 +0200

    Added Accounts Database
    Added a new ASP .NET Project and created an initial migration for generating the database

---
+++ Ticketist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ticketist.Startup))]
+namespace Ticketist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
